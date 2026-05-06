484665c357ac16abba2e6813b5a97d062bab8e03
Filippo Savi <filippo.savi@unimore.it>
2026-05-06 15:08:18 +0000

    added implementation of a simple source mapping engine to keep track of real module definition path through the preprocessor

---
+++ includes/frontend/analysis/preprocessor/source_mapper.hpp
@@ -0,0 +1,44 @@
+//  Copyright 2026 Filippo Savi
+//  Author: Filippo Savi <filssavi@gmail.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+#ifndef ANANKE_SOURCE_MAPPER_HPP
+#define ANANKE_SOURCE_MAPPER_HPP
+
+#include <string>
+#include <vector>
+#include <optional>
+
+class source_mapper {
+public:
+    struct source_range {
+        unsigned int start_line;
+        unsigned int end_line;
+        std::string source_path;
+    };
+    std::optional<std::string> get_path(unsigned int line_n);
+    void add_range(unsigned int start, unsigned int stop, const std::string &path);
+    void open_range(unsigned int line_n,const std::string &path);
+    void close_range(unsigned int line_n);
+private:
+    std::vector<source_range> map;
+    std::vector<source_range> ranges_stack;
+    source_range current_range;
+    bool constructing_range = false;
+
+};
+
+
+#endif //ANANKE_SOURCE_MAPPER_HPP
---
+++ src/frontend/analysis/preprocessor/source_mapper.cpp
@@ -0,0 +1,49 @@
+//  Copyright 2026 Filippo Savi
+//  Author: Filippo Savi <filssavi@gmail.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+#include "frontend/analysis/preprocessor/source_mapper.hpp"
+
+
+std::optional<std::string> source_mapper::get_path(unsigned int line_n) {
+    for (auto &[start, stop, path]: map) {
+        if (line_n>start && line_n < stop) return path;
+    }
+    return std::nullopt;
+}
+
+void source_mapper::add_range(unsigned int start, unsigned int stop, const std::string &path) {
+    map.emplace_back(start, stop, path);
+}
+
+void source_mapper::open_range(unsigned int line_n, const std::string &path) {
+    if (constructing_range) {
+        ranges_stack.push_back(current_range);
+    }
+    current_range = {line_n, line_n, path};
+    constructing_range = true;
+}
+
+void source_mapper::close_range(unsigned int line_n) {
+    if (ranges_stack.empty()) {
+        constructing_range = false;
+    } else {
+        current_range = ranges_stack.back();
+        ranges_stack.pop_back();
+    }
+    current_range.end_line = line_n;
+    map.push_back(current_range);
+    current_range = {};
+}
