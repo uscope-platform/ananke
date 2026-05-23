f78ce843cc06da6c974a39ddfc620c7c6624c4f8
Filippo Savi <filippo.savi@unimore.it>
2026-05-23 09:13:57 +0000

    added class for hdl integer representation

---
+++ includes/data_model/HDL/parameters/common/hdl_integer.hpp
@@ -0,0 +1,32 @@
+//  Copyright 2026 Filippo Savi
+//  Author: Filippo Savi <filssavi@gmail.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+#ifndef ANANKE_HDL_INTEGER_HPP
+#define ANANKE_HDL_INTEGER_HPP
+
+#include <cstdint>
+
+class hdl_integer {
+public:
+private:
+    int64_t value = 0;
+    uint64_t size = 0;
+    bool signedness = false;
+};
+
+
+
+#endif //ANANKE_HDL_INTEGER_HPP
