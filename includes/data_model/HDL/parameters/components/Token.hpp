bf46fdd1ba73eb5b06659c90e0532d3913402bd5
filss <filssavi@gmail.com>
2026-06-12 21:03:07 +0200

    add token class that will with time replace the Expression_Component hack

---
+++ includes/data_model/HDL/parameters/components/Token.hpp
@@ -0,0 +1,26 @@
+//  Copyright 2026 Filippo Savi
+//  Author: Filippo Savi <filssavi@gmail.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef ANANKE_TOKEN_HPP
+#define ANANKE_TOKEN_HPP
+
+#include "data_model/HDL/parameters/components/Parameter_value_base.hpp"
+
+class Token : public Parameter_value_base{
+
+};
+
+
+#endif //ANANKE_TOKEN_HPP
